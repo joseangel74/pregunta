6d35a9b127fc5efbb74d0cf88c76b413ba24eb34
joseangel74 <mamanipinto.8470077@gmail.com>
2024-07-02 23:36:35 -0400

    componente ejercicio realizado

---
+++ src/componentes /Ejercicio.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Ejercicio.css";
+import Info from "./Info";
+
+function Ejercicio() {
+  return (
+    <div className="ejercicio-container">
+      <img src="https://media.revistagq.com/photos/5d35929de887bb000828e8f3/16:9/w_2560%2Cc_limit/GettyImages-982408932-(1).jpg" alt="" />
+      <Info />
+    </div>
+  );
+}
+
+export default Ejercicio;
