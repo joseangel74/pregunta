713f4174980b7a285a15534fd88886d388b25159
joseangel74 <mamanipinto.8470077@gmail.com>
2024-07-02 23:39:56 -0400

    componente info  realizado

---
+++ src/componentes /Info.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Info.css";
+
+function Info() {
+  return (
+    <div className="container-info">
+      <h1>Flexiones</h1>
+      <p>Descripcion de flexiones</p>
+    </div>
+  );
+}
+
+export default Info;
