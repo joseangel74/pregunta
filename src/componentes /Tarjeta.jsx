12810447e4b240ee672ea735506debd88cab2a91
joseangel74 <mamanipinto.8470077@gmail.com>
2024-07-02 23:42:34 -0400

    componente tarjeta realizado

---
+++ src/componentes /Tarjeta.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Tarjeta.css";
+import Ejercicio from "./Ejercicio";
+
+function Tarjeta() {
+  return (
+    <div className="tarjeta-container">
+      <Ejercicio />
+    </div>
+  );
+}
+
+export default Tarjeta;
