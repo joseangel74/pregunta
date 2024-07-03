b86519c88b530f7cb53a1a919703b14f91c31829
joseangel74 <mamanipinto.8470077@gmail.com>
2024-07-02 23:45:30 -0400

    componente tarjetas realizado

---
+++ src/componentes /Tarjetas.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Tarjetas.css";
+import Tarjeta from "./Tarjeta";
+
+function Tarjetas() {
+  return (
+    <div className="tarjetas-container">
+      <h1>Tarjetas</h1>
+      <Tarjeta />
+    </div>
+  );
+}
+
+export default Tarjetas;
