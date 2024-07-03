286f272d37911ec670cde989fc1a4457e9336c3f
joseangel74 <mamanipinto.8470077@gmail.com>
2024-07-02 23:52:36 -0400

    componente apps realizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
+import Tarjeta from './componentes/Tarjeta'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <Tarjeta/>
     </>
   )
 }
